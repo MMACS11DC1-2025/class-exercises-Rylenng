33f98ebfbc3fbe459cc4878c074528efd5f6175a
Rylenng <1284479@learn.vsb.bc.ca>
2025-09-29 21:25:10 +0000

    changes

--- 2.5/comparing.py
+++ 2.5/comparing.py
@@ -6,3 +6,30 @@ You may use user input to add interactivity to the program.
 You must design your algorithm in English first, then translate it to Python code.
 Test as you go! Describe in your comments what steps you took to test your code.
 """
+#compare
+#rylen ng
+#sept 26
+answer = input()
+file = open("2.4/responses.csv")
+junk = file.readline()
+
+basketball = 0
+hockey = 0
+baseball = 0
+
+for data in file:
+
+    datalist = data.split(",")
+
+print(datalist[4])
+
+if answer == "basketball":
+    basketball += 1
+elif answer == "baseball":
+    baseball += 1
+elif answer == "hockey":
+    hockey += 1
+
+print("basketball: " + str(basketball))
+print("hockey: " + str(hockey))
+print("baseball: " + str(baseball))
