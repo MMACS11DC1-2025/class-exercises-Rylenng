594dce9d079adf39df31d3753db5c4ae23d4876e
Rylenng <1284479@learn.vsb.bc.ca>
2025-11-14 21:35:07 +0000

    changes

--- 5.1/image_open.py
+++ 5.1/image_open.py
@@ -2,15 +2,18 @@ from PIL import Image
 
 def is_green(r,g,b):
     if r >= 0 and r <= 100 and g >= 200 and g <= 255 and b >= 0 and b <= 100:
-        return True
+        return "green"
     else:
-        return False
-image_green = Image.open("CS11/5.1/kid-green.jpg").load()
-image_beach = Image.open("CS11/5.1/beach.jpg").load()
+        return "not green"
+image_green = Image.open("5.1/kid-green.jpg").load()
+image_beach = Image.open("5.1/beach.jpg").load()
 
-pixel = image_green[0,0]
-r = pixel[0, 0][0]
-g = pixel[0, 0][1]
-b = pixel[0, 0][2]
+image_output = Image.open("5.1/kid-green.jpg")
 
-print(is_green(r,g,b))
+width = image_output.width
+length = image_output.height
+for i in range(width):
+    for j in range(height):
+        r, g, b = image_green[i, j]
+        if is_green(r, g, b) == "green":
+            image_output.putpixel((i, j), image_beach[i, j])
